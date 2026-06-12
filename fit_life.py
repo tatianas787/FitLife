dfe3abe4617c9ee77ea24b8b9e17f95b03c831d1
Tatiana Skirnevskaya <tatiskir@gmail.com>
2026-06-12 23:17:52 +0300

    текст комментария

--- fit_life.py
+++ fit_life.py
@@ -27,5 +27,5 @@ water_needed = round(user_weight * WATER_PER_KG_ML / 1_000, 1)
 # 4. Вывод красивого результата
 print(f'Расчёт для пользователя, {user_name.title()}, возраст: {user_age} лет')
 print(f'Индекс Массы Тела: {bmi}')
-print(f'Ваша ежедневная норма воды:  {water_needed} л.')
+print(f'Ваша ежедневная норма воды: {water_needed} л.')
 print("Расчет окончен. Будьте здоровы!")
